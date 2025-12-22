be160a2d1408734ace097e35c7a58d5a906891df
Eric Fournié <efournie@gmail.com>
2025-12-22 17:42:12 +0100

    Moved main code to main(), seems to fix a bug preventing printing messages to stdout

--- src/kstars-watchdog.py
+++ src/kstars-watchdog.py
@@ -40,62 +40,66 @@ def attempt_bus_connect(bus, service_name, path, max_retries=10, error_callback=
     log(f'{service_name} {path} connected successfully.')
     return result
 
-home = Path.home()
-default_sched_job = os.path.join(home, 'default.esl')
-max_retries = 10
+def main():
+    home = Path.home()
+    default_sched_job = os.path.join(home, 'default.esl')
+    max_retries = 10
 
-# Read command line arguments
-parser = argparse.ArgumentParser(description='KStars / Ekos watchdog program, automatically resuming in case of a crash.')
-parser.add_argument('-k', '--kstars_executable', type=str, default='/usr/bin/kstars', help='Full path to the kstars executable.')
-parser.add_argument('-s', '--sched_job', type=str, default=default_sched_job, help='Path to the default Ekos scheduler job file to be resumed in case of a crash.')
-args = parser.parse_args()
+    # Read command line arguments
+    parser = argparse.ArgumentParser(description='KStars / Ekos watchdog program, automatically resuming in case of a crash.')
+    parser.add_argument('-k', '--kstars_executable', type=str, default='/usr/bin/kstars', help='Full path to the kstars executable.')
+    parser.add_argument('-s', '--sched_job', type=str, default=default_sched_job, help='Path to the default Ekos scheduler job file to be resumed in case of a crash.')
+    args = parser.parse_args()
 
-# Initialize DBus
-bus = SessionBus()
+    # Initialize DBus
+    bus = SessionBus()
 
-# Main loop
-while True:
-    # Check if KStars is running, try to start it otherwise
-    ekos = attempt_bus_connect(bus, 'org.kde.kstars', '/KStars/Ekos', error_callback=start_kstars)
-    if ekos == None:
-        log(f'KStars could not be started, giving up...')
-        exit(-1)
-    log('KStars is running.')
-
-    # Show main Ekos window
-    ekos.start()
-    show_ekos_action = bus.get('org.kde.kstars', '/kstars/MainWindow_1/actions/show_ekos')
-    show_ekos_action.trigger()
-
-    # INDI devices should be up and running by now
-    if os.path.exists(args.sched_job):
-        log('Scheduler job file present, unparking telescope')
-        # Unpark mount
-        mount = attempt_bus_connect(bus, 'org.kde.kstars','/KStars/Ekos/Mount')
-        if mount is not None:
-            if mount.status != 4:
-                # Mount is not parked, park it before unparking it otherwise job may not restart correctly
-                mount.park()
-                time.sleep(20)
-            mount.unpark()
-        else:
-            log('Could not unpark mount')
-
-        # (Re-)start scheduler jobs : load and start scheduler file
-        scheduler = attempt_bus_connect(bus, 'org.kde.kstars','/KStars/Ekos/Scheduler')
-        if scheduler is not None:
-            scheduler.loadScheduler(args.sched_job)
-            scheduler.start()
-        else:
-            log('Could not reload scheduler job')
-
-    # Watchdog loop, restart everything if Kstars crashes or is closed
-    log('Everything is up and running, initiating watchdog.')
+    # Main loop
     while True:
-        try:
-            ekos = bus.get('org.kde.kstars','/KStars/Ekos')
-        except:
-            log('Kstars has been closed or crashed, restarting in 5 seconds...')
-            time.sleep(5)
-            break
-        time.sleep(1)
+        # Check if KStars is running, try to start it otherwise
+        ekos = attempt_bus_connect(bus, 'org.kde.kstars', '/KStars/Ekos', error_callback=start_kstars)
+        if ekos == None:
+            log(f'KStars could not be started, giving up...')
+            exit(-1)
+        log('KStars is running.')
+
+        # Show main Ekos window
+        ekos.start()
+        show_ekos_action = bus.get('org.kde.kstars', '/kstars/MainWindow_1/actions/show_ekos')
+        show_ekos_action.trigger()
+
+        # INDI devices should be up and running by now
+        if os.path.exists(args.sched_job):
+            log('Scheduler job file present, unparking telescope')
+            # Unpark mount
+            mount = attempt_bus_connect(bus, 'org.kde.kstars','/KStars/Ekos/Mount')
+            if mount is not None:
+                if mount.status != 4:
+                    # Mount is not parked, park it before unparking it otherwise job may not restart correctly
+                    mount.park()
+                    time.sleep(20)
+                mount.unpark()
+            else:
+                log('Could not unpark mount')
+
+            # (Re-)start scheduler jobs : load and start scheduler file
+            scheduler = attempt_bus_connect(bus, 'org.kde.kstars','/KStars/Ekos/Scheduler')
+            if scheduler is not None:
+                scheduler.loadScheduler(args.sched_job)
+                scheduler.start()
+            else:
+                log('Could not reload scheduler job')
+
+        # Watchdog loop, restart everything if Kstars crashes or is closed
+        log('Everything is up and running, initiating watchdog.')
+        while True:
+            try:
+                ekos = bus.get('org.kde.kstars','/KStars/Ekos')
+            except:
+                log('Kstars has been closed or crashed, restarting in 5 seconds...')
+                time.sleep(5)
+                break
+            time.sleep(1)
+
+if __name__ == '__main__':
+    main()
